d8254c6d7065ba0218688d84ac0923f58098e479
ekisu <dts.ramon@gmail.com>
2025-10-18 21:04:02 -0300

    feat: add NixOS module

--- flake.nix
+++ flake.nix
@@ -2,18 +2,41 @@
   description = "A simple Go development shell";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs = { self, nixpkgs, flake-parts }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
+      flake = {
+        nixosModules = {
+          gamemode-auto-enable = moduleWithSystem (
+            perSystem@{ pkgs, system, ... }:
+            nixos@{ config, lib, ... }: {
+              options.services.gamemode-auto-enable = {
+                enable = lib.mkEnableOption "gamemode-auto-enable user service";
+              };
+
+              config = lib.mkIf config.services.gamemode-auto-enable.enable {
+                systemd.user.services.gamemode-auto-enable = {
+                  description = "Automatically enable GameMode when a game is running.";
+                  partOf = [ "graphical-session.target" ];
+                  wantedBy = [ "graphical-session.target" ];
+                  serviceConfig = {
+                    ExecStart = "${self.packages.${system}.gamemode-auto-enable}/bin/gamemode-auto-enable";
+                    Restart = "on-failure";
+                    RestartSec = "5s";
+                  };
+                };
+              };
+            }
+          );
+
+          default = self.nixosModules.gamemode-auto-enable;
         };
-      in
-      {
+      };
+
+      perSystem = { config, pkgs, self', ... }: {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             gcc
@@ -29,9 +52,11 @@
             src = self;
             vendorHash = "sha256-r9FG8gtQzkf/Idb1b8TrSoyoxBtwFTKosqeeaBRhiTQ=";
           };
-          default = self.packages.${system}.gamemode-auto-enable;
+          default = self'.packages.gamemode-auto-enable;
         };
+      };
 
-        nixosModules = {};
-       });
+      systems = [ "x86_64-linux" ];
+    });
+        
 }
